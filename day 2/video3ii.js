b21c62e9d21e3f650f2528e546f8e6085da881d1
Fazil Niyazdeen TM <113542759+Fazilniyaz@users.noreply.github.com>
2026-09-04 11:58:22 +0530

    commit student

---
+++ day 2/video3ii.js
@@ -0,0 +1,12 @@
+var student = {
+  name: "Alice",
+  age: 20,
+  grade: "A",
+  marks: {
+    math: 95,
+    science: 90,
+    english: 88,
+  },
+};
+console.log(student.age);
+console.log(student.marks.math);
