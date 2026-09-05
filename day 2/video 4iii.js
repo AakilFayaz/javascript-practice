b35d7e7940f4499085895b232eab0a1087e03261
Fazil Niyazdeen TM <113542759+Fazilniyaz@users.noreply.github.com>
2026-09-05 13:58:36 +0530

    splice operator

--- day 2/video 4iii.js
+++ day 2/video 4iii.js
@@ -2,3 +2,5 @@ var arr = [1, 2, 3];
 var arr1 = [4, 5, 6];
 var arr2 = [7, 8, 9];
 console.log(arr.concat(arr1, arr2));
+console.log(ResizeObserverSize.array);
+)
---
+++ day 2/video4v.js
@@ -0,0 +1,3 @@
+var arr = [1, 2, 3, 4, 5];
+arr.splice(2, 3, 100, 200, 300);
+console.log(arr);
