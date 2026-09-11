a82b911d08477020bc75f2ed89062d4c7bfc31f2
Fazil Niyazdeen TM <113542759+Fazilniyaz@users.noreply.github.com>
2026-09-11 19:52:57 +0530

    missing no

--- day 3/missingnoiv.js
+++ day 3/missingnoiv.js
@@ -7,3 +7,13 @@ for (var i = 1; i <= 100; i++) {
   }
 }
 console.log(arr.length);
+for (i=0;i<=100;i++){
+  if (arr[i]+1==arr[i+1]){
+    continue;
+  }
+else{
+    console.log("The missing number is: ",arr[i]+1);
+    break;
+           
+  }
+}
