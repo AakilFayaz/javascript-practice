893f00f7a7b4c3b0fba6947396cdaf1c58efca35
Fazil Niyazdeen TM <113542759+Fazilniyaz@users.noreply.github.com>
2026-09-18 19:29:46 +0530

    sumarrtar

---
+++ day 4/sumarrtar.js
@@ -0,0 +1,9 @@
+let arr = [1, 2, 3, 4, 5, 6, 7];
+let target = 9;
+for (let i = 0; i < arr.length; i++) {
+  for (let j = i + 1; j < arr.length; j++) {
+    if (arr[i] + arr[j] === target) {
+      console.log("The pair is:", arr[i], "+", arr[j], "=", target);
+    }
+  }
+}
