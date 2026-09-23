a679f8beba25b6c28933dc4f32dd81581c1ac5ac
Fazil Niyazdeen TM <113542759+Fazilniyaz@users.noreply.github.com>
2026-09-23 19:36:25 +0530

    first non repeated char

---
+++ day 5/nonrepeatedchar.js
@@ -0,0 +1,19 @@
+let word = "fazil";
+let firstnonrepeatedchar = "";
+for (let i = 0; i < word.length; i++) {
+  let count = 0;
+
+  for (let j = 0; j < word.length; j++) {
+    if (word[i] === word[j]) {
+      count++;
+    }
+  }
+  if (count === 1) {
+    firstnonrepeatedchar = word[i];
+    break;
+  }
+}
+
+console.log(firstnonrepeatedchar);
+console.log(firstnonrepeatedchar);
+// it only prints first non repeated character in the string
